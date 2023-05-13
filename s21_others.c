89eac16555e5560c29318c306b85e3f868cd78c3
Ginger_mustache <artzaneg@gmail.com>
2023-05-13 19:46:58 +0300

    need to add more tests

--- s21_others.c
+++ s21_others.c
@@ -19,6 +19,7 @@ int s21_transpose(matrix_t *A, matrix_t *result) {
   return output;
 }
 
+// Матрица алгебраических дополнений
 int s21_calc_complements(matrix_t *A, matrix_t *result) {
   int output = CONVERSATION_OK;
   int check_null = 0;
@@ -40,6 +41,42 @@ int s21_calc_complements(matrix_t *A, matrix_t *result) {
             output = CONVERSATION_ERROR;
           }
         }
+      }
+    } else {
+      output = CALCULATION_ERROR;
+    }
+  } else {
+    output = CONVERSATION_ERROR;
+  }
+  return output;
+}
+
+// Обратная матрица
+int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
+  int output = 0;
+  double determinant = 0.0;
+  matrix_t alg_add = {0};        // Надо потом почистить
+  matrix_t transpose_mat = {0};  // Надо потом почистить
+
+  if (!s21_check_matrix(A)) {
+    output = s21_create_matrix(A->rows, A->columns, result);
+    if (A->rows == 1 && A->columns == 1) {
+      result->matrix[0][0] = 1.0 / A->matrix[0][0];
+      return output = CONVERSATION_OK;
+    } else if (A->rows == A->columns) {
+      s21_determinant(A, &determinant);
+      if (determinant) {
+        output += s21_create_matrix(A->rows, A->columns, &alg_add);
+        output += s21_create_matrix(A->rows, A->columns, &transpose_mat);
+        output += s21_calc_complements(A, &alg_add);
+        output += s21_transpose(&alg_add, &transpose_mat);
+        if (output == 0) {
+          s21_mult_number(&transpose_mat, (1 / determinant), result);
+        }
+        s21_remove_matrix(&alg_add);
+        s21_remove_matrix(&transpose_mat);
+      } else {
+        output = CALCULATION_ERROR;
       }
     } else {
       output = CALCULATION_ERROR;
