dbebf70329f748a0212e7210271ecdb20eec58ed
Ginger_mustache <artzaneg@gmail.com>
2023-05-13 21:34:55 +0300

    need test in mini verter and check leaks

--- s21_others.c
+++ s21_others.c
@@ -55,8 +55,6 @@ int s21_calc_complements(matrix_t *A, matrix_t *result) {
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
   int output = 0;
   double determinant = 0.0;
-  matrix_t alg_add = {0};        // Надо потом почистить
-  matrix_t transpose_mat = {0};  // Надо потом почистить
 
   if (!s21_check_matrix(A)) {
     output = s21_create_matrix(A->rows, A->columns, result);
@@ -66,6 +64,8 @@ int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
     } else if (A->rows == A->columns) {
       s21_determinant(A, &determinant);
       if (determinant) {
+        matrix_t alg_add = {0};
+        matrix_t transpose_mat = {0};
         output += s21_create_matrix(A->rows, A->columns, &alg_add);
         output += s21_create_matrix(A->rows, A->columns, &transpose_mat);
         output += s21_calc_complements(A, &alg_add);
