63c8dcbb077a6a0204698cecf53faa8aeaed644c
Ginger_mustache <artzaneg@gmail.com>
2023-05-18 11:38:06 +0300

    remake comparison and add misk

--- src/s21_comparison_matrix.c
+++ src/s21_comparison_matrix.c
@@ -1,14 +1,15 @@
 #include "s21_matrix.h"
 
+// Сравнение матриц
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
   int output = COMP_SUCCESS;
 
-  if (A->columns == B->columns && A->rows == B->rows) {
-    for (int i = 0; i < A->rows && output == COMP_SUCCESS; i++) {
-      for (int j = 0; j < A->columns && output == COMP_SUCCESS; j++) {
-        if (A->matrix[i][j] == B->matrix[i][j]) {
-          output = COMP_SUCCESS;
-        } else {
+  if (!s21_check_matrix(A) && !s21_check_matrix(B) &&
+      A->columns == B->columns && A->rows == B->rows) {
+    for (int i = 0; i < A->rows && output; i++) {
+      for (int j = 0; j < A->columns && output; j++) {
+        if (round(A->matrix[i][j] * pow(10, 7)) !=
+            round(B->matrix[i][j] * pow(10, 7))) {
           output = COMP_FAILURE;
         }
       }
@@ -16,6 +17,5 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
   } else {
     output = COMP_FAILURE;
   }
-
   return output;
 }
